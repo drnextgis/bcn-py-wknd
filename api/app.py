da049a5cabd2a2dbfe8ea96ba989f1a02cbd171f
vlado <vladimir.chernenko@kiwi.com>
2019-11-09 12:20:05 +0100

    Initial commit

---
+++ api/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, jsonify, request
+from dateutil.parser import parse as date_parse
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/search", methods=["post"])
+def search():
+    # searchForm: {
+    #     departure: new Date(2019, 9, 16),
+    #     return: new Date(2096, 9, 16),
+    #     from: '',
+    #     to: ''
+    # }
+    searchForm = request.get_json()
+
+    parsedSearchForm = {
+        "from": searchForm["from"],
+        "to": searchForm["to"],
+        "from_date": date_parse(searchForm["departure"]),
+        "to_date": date_parse(searchForm["return"]),
+    }
+
+    search_results = [
+        {
+            "from": "BCN",
+            "to": "PRG",
+            "departure": "2019, 9, 16",
+            "return": "2096, 9, 16",
+        },
+        {
+            "from": "BCN",
+            "to": "PRG",
+            "departure": "2019, 9, 17",
+            "return": "2096, 9, 18",
+        },
+    ]
+    return jsonify(results=search_results)
+
+
+if __name__ == "__main__":
+    app.run(port=8080)
